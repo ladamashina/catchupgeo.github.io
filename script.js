f863f05c5339e6b4e784e366022d1371a121daad
maksim.sarvas <ladamashina@gmail.com>
2017-11-23 12:07:24 +0300

    fix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />
+//<uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />
 function startGeoGame() {
   geoFindMe();
   StartStop();
