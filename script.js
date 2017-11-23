1ea9918b203135222179fc6d31ed04a43cf1b23b
maksim.sarvas <ladamashina@gmail.com>
2017-11-23 10:34:41 +0300

    fix calculator

--- script.js
+++ script.js
@@ -58,13 +58,9 @@ function chekDistance(cordinatsArray) {
   var R = 6371; // Radius of the earth in km
   var dLat = deg2rad(x2-x1);  // deg2rad below
   var dLon = deg2rad(y2-y1); 
-  var a = 
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ; 
+  var a =     Math.sin(dLat/2) * Math.sin(dLat/2) +  Math.cos(deg2rad(x1)) * Math.cos(deg2rad(x2)) *     Math.sin(dLon/2) * Math.sin(dLon/2)  ; 
   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var d = R * c /1000; // Distance in km
+  var d = R * c*1000; // Distance in km
   return d;
 }
 
